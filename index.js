068c0adf72021020129bfcf2593ab0d573b41262
Pedro <predoisac@gmail.com>
2025-09-15 21:36:19 -0300

    exercicio 1 eu sou um genio

--- index.js
+++ index.js
@@ -10,3 +10,10 @@ app.get("/api/pessoa/:id", (req, res) => {
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000");
 })
+
+app.get("/exercicio1/:num1/:num2",(req, res) => {
+    const num1 = parseInt(req.params.num1);
+    const num2 = parseInt(req.params.num2);
+
+    res.status(200).send(num1 + "+" + num2 + "=" + (num1 + num2))
+})
